3edc2dbd7543004b588d0a518fda196575c75356
Seyoung Hong <maiorem00@gmail.com>
2026-05-15 15:11:05 +0900

    feat: 로깅 Adviser 구현

--- src/main/java/com/baedal/support/SupportController.java
+++ src/main/java/com/baedal/support/SupportController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 public class SupportController {
 
     private final ChatClient.Builder builder;
+    private final PerformanceLoggingAdvisor performanceLoggingAdvisor;
 
     private static final ObjectMapper LENIENT_MAPPER = new ObjectMapper()
             .configure(JsonParser.Feature.ALLOW_UNQUOTED_CONTROL_CHARS, true);
@@ -26,6 +27,7 @@ public class SupportController {
     public SupportResponse triage(@RequestBody ChatRequest req) {
         var converter = new BeanOutputConverter<>(SupportResponse.class, LENIENT_MAPPER);
         return builder.defaultSystem(BaedalPrompt.SYSTEM_PROMPT)
+                .defaultAdvisors(performanceLoggingAdvisor)
                 .build()
                 .prompt()
                 .user(req.message())
